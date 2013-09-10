0d6e27520f573c2ca0445e8c1b468d84aad33e0d
Johan Jatko <johan@pie-studios.com>
2013-09-10 19:57:17 +0200

    Fixed bug in servicemgr that skipped one cmdline param
    [ci skip]

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
-service test start
+sudo service test start
