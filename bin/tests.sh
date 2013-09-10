9048b4f22d65c3cd300362353f039193ddca0fbe
Johan Jatko <johan@pie-studios.com>
2013-09-10 20:22:16 +0200

    New travis test

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
-sudo service test start
+sudo servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
+sudo -E service test start
