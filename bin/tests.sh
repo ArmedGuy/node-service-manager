cd7072becbff65b9a3bf67a1651a7239573b4e73
Johan Jatko <johan@pie-studios.com>
2013-09-10 20:56:00 +0200

    Lets try this
    [ci skip]

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
+servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end(\"Faggotini is a cheese\");}).listen(1337);"
 service test start
 if wget -qO - http://pie-studios.com | grep -cim1 Faggotini -eq "1"; then
 	exit 0;
