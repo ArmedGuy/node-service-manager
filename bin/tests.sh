537328ae61aefd0b171fe17ebcbeba19813e392c
Johan Jatko <johan@pie-studios.com>
2013-09-10 23:43:47 +0200

    test script done

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end(\"Faggotini is a cheese\");}).listen(1337);"
+servicemgr install test node "-e" "require('http').createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end(\"Faggotini is a cheese\");}).listen(1337);"
 service test start
-if wget -qO - http://pie-studios.com | grep -cim1 Faggotini -eq "1"; then
-	exit 0;
+FIND=$(wget -qO - http://127.0.0.1:1337/ | grep -cim1 Faggotini)
+if [ "$FIND" -eq "1" ]; then
+    exit 0;
 else
-	exit 1;
-fi;
+	echo "Did not recieve correct response from server!"
+    exit 1;
+fi
