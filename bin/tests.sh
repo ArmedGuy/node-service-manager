16ad21541d5f6da9e6809a3c10a6e2232c398c07
Johan Jatko <johan@pie-studios.com>
2013-09-10 20:37:32 +0200

    Fixed is_root in linux-controller

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo -E servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
-sudo -E service test start
+servicemgr install test node "-e" "var http=require('http');http.createServer(function(req,res){ res.writeHead(200,{'Content-Type': 'text/plain'});res.end('Faggotini is a cheese\n');}).listen(1337);"
+service test start
